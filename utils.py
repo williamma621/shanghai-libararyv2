61ddbc01926aefba7cd79a0b3874b174fc4910f0
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2022-09-10 12:44:40 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -36,6 +36,49 @@ def des_list_C():
         # print(ind)
         f.write(res)
 
+def info_C_list():
+
+    with open("infoC.txt", 'w') as f:
+        res = "["
+        lines = buildings
+        ind = 0
+        for i in lines:
+
+            temp = "{"
+
+            #get name
+            name = i['nameS']
+            temp += "name:'" + name + "', "
+
+            # get description
+            des = i['des']
+            if des == "":
+                des = i['address']
+            # print(des)
+            else:
+                des = des.replace('\n', '')
+                des = des.replace('\t', '')
+            temp += "des:'" + des + "', "
+
+            # get longitude and latitude
+            y = i['long']
+            x = i['lat']
+            if len(y) == 0 or len(x) == 0:
+                continue
+            temp += "y:'" + y + "', "
+            temp += "x:'" + x + "'"
+            temp += "}"
+
+            res += temp + ", "
+            ind += 1
+            if ind%10 == 0: res += '\n'
+        res = res[:-2] + "]"
+        print(ind)
+        f.write(res)
+
+
+
 if __name__ == "__main__":
     # name_list_C()
-    des_list_C()
+    # des_list_C()
+    info_C_list()
