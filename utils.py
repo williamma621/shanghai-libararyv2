c548ceb86a2b5cac88d894c4faafc56c123343bd
Fuyi Yang <75815872+Fuyi-Yang@users.noreply.github.com>
2022-09-10 12:14:35 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,32 +1,41 @@
 from buildings import buildings
 import pickle
 
-def name_list():
+def name_list_C():
     with open("names.txt", 'w') as f:
         res = "["
         lines = buildings
+        ind = 0
         for i in lines:
+            ind += 1
             name = i['nameS']
             res += "'" + name + "', "
+            if ind%100 == 0: res += '\n'
+        print(ind)
         res += "]"
+        
         f.write(res)
 
-def des_list():
-    with open("des.txt", 'w') as f:
+def des_list_C():
+    with open("desC.txt", 'w') as f:
         res = "["
         lines = buildings
         ind = 0
         for i in lines:
             ind += 1
             des = i['des']
+            if des == "":
+                des = i['address']
             # print(des)
-            des = des.replace('\n', '')
-            des = des.replace('\t', '')
+            else:
+                des = des.replace('\n', '')
+                des = des.replace('\t', '')
             res += "'" + des + "', "
-            if ind%10 == 0: res += '\n'
+            if ind%9 == 0: res += '\n'
         res += "]"
+        # print(ind)
         f.write(res)
 
 if __name__ == "__main__":
-    # name_list()
-    des_list()
+    # name_list_C()
+    des_list_C()
